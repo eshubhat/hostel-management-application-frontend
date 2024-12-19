64c6d83e640e420979765f20b66abac2301524f9
Eshan A. Bhat <eshubhat03@gmail.com>
2024-12-19 11:34:45 +0530

    vite+tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent font-bold text-3xl">
+        HOSTEL MANAGEMENT APPLICATION
+      </div>
+    </>
+  );
+}
+
+export default App;
